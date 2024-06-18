27cd5d4aae963dda5ec4e2b4edb90e29bde3284c
Martti Soininen <martti.soininen@evident.fi>
2024-06-18 12:35:41 +0300

    Add nav, personnel page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,31 @@
 import {useLocation} from "@solidjs/router";
+import {For} from "solid-js";
+
+interface Link {
+    url: string,
+    label: string,
+}
 
 export default function Nav() {
     const location = useLocation();
     const active = (path: string) =>
-        path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
+        path == location.pathname ? "bg-[#898a1f]" : "";
+    const item: string = "p-2 pt-8 pb-2";
+    const links: Link[] = [
+        {url: "/", label: "Evident"},
+        {url: "/customers", label: "Töitämme"},
+        {url: "/personnel", label: "Henkilöstö"},
+        {url: "/opensource", label: "Open source"},
+        {url: "/contact", label: "Yhteystiedot"},
+    ];
+
     return (
-        <nav class="bg-sky-800">
-            <ul class="container flex items-center p-3 text-gray-200">
-                <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-                    <a href="/">Home</a>
-                </li>
-                <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-                    <a href="/about">About</a>
-                </li>
-            </ul>
+        <nav class="bg-[#585812]">
+            <div class="container flex items-center text-white px-10">
+                <For each={links}>{(link) =>
+                    <a class={`${item} ${active(link.url)}`} href={link.url}>{link.label}</a>
+                }</For>
+            </div>
         </nav>
     );
 }
---
+++ src/routes/personnel.tsx
@@ -0,0 +1,88 @@
+import {For, JSXElement} from "solid-js";
+
+interface Dude {
+    name: string;
+    picture: string;
+    title: string;
+    description: JSXElement;
+}
+
+const personnel: Dude[] = [
+    {
+        name: "Henrik Huttunen",
+        title: "Ohjelmistokehittäjä",
+        picture: "faces/hhuttunen2_small.jpg",
+        description: "Innostun uuden luomisesta ja ongelmien ratkaisemisesta. Pyrin tekemään vankasti toimivia ratkaisuja, joita on sekä miellyttävä käyttää että jatkokehittää.",
+    },
+    {
+        name: "Jussi Koiranen",
+        title: "Ohjelmistokehittäjä",
+        picture: "faces/jkoiranen_small.jpg",
+        description: "Minulla on yli kahdenkymmenen vuoden työkokemus vaihtelevista ohjelmistohankkeista. Nautin aidosti työstäni ja pyrin jatkuvasti oppimaan uutta, mikä ei kuitenkaan tarkoita jatkuvasti uuden teknologian perässä juoksemista. Viihdyn erityisesti projekteissa, jotka haastavat pohtimaan ratkaisuja yhteistyössä eri alojen asiantuntijoiden kanssa. Toinen mielenkiinnon kohteeni on vanhojen sovellusten inkrementaalinen modernisointi ja kehittäminen."
+    },
+    {
+        name: "Juha Komulainen",
+        title: "Arkkitehti",
+        picture: "faces/komu_small.jpg",
+        description: <>
+            <p>Olen kokenut ohjelmistoarkkitehti Tampereelta. Koodaaminen on ollut osa elämääni jo lapsuudesta asti.
+                Vuodesta 2001 asti olen toiminut ammattilaisena, suunnitellen ja toteuttaen monipuolisia
+                ohjelmistoprojekteja. Kokemukseni kattaa kaiken sulautetuista järjestelmistä web-sivuihin,
+                verkonhallinnasta ERP-järjestelmiin ja kääntäjiin. Nykyisessä työssäni toimin pääsuunnittelijana ja
+                vastaan niin pienistä teknisistä yksityiskohdista kuin arkkitehtuurin kokonaiskuvasta. Olen myös pitänyt
+                alan koulutuksia ja tehnyt auditointeja, jakaen osaamistani eteenpäin.</p>
+            <p>Julkisia projektejani löydät sivuiltani <a href="https://komu.dev">komu.dev</a> ja <a
+                href="https://github.com/komu">GitHubista</a>.</p>
+        </>
+    },
+    {
+        name: "Janne Mäki",
+        title: "Ohjelmistokehittäjä",
+        picture: "faces/jmaki_small.jpg",
+        description: "Pidän tärkeänä pysyä kehityksessä ajan hermolla ja opiskella uusia asioita ja teknologioita. Oman osaamiseni kehittäminen onkin minulle myös tärkeä harrastus polkupyöräilyn ohella. Teknologiat eivät kuitenkaan ole hopealuoti ohjelmistokehityksen ongelmiin, koska kaikki alkaa tarpeiden syvällisestä ymmärtämisestä ja selkeän ylläpidettävän koodin tuottamisesta."
+    },
+    {
+        name: "Tero Poikonen",
+        title: "Ohjelmistokehittäjä",
+        picture: "faces/tpoikonen2_small.jpg",
+        description: "Olen tehnyt arkkitehtuurikonsultointia ja määrittelytyötä. Olen työskennellyt ohjelmistosuunnittelijana, projektipäällikkönä ja loppukäyttäjien koulutuksessa. Olen kirjoittanut käyttöohjeita ja teknistä dokumentaatiota."
+    },
+    {
+        name: "Martti Soininen",
+        title: "Ohjelmistokehittäjä",
+        picture: "faces/msoininen_small.jpg",
+        description: "Olen koodannut web-sovelluksia ammatikseni vuodesta 2008 (full-stack). Yritän aina selvittää ongelman perimmäisen syyn ja ratkaista sen. Saatan uppoutua mielenkiintoisiin algoritmisiin ongelmiin pitkäksikin aikaa, mutta pidän myös tiimityöskentelystä ja olen toiminut tutorinakin. Toisten ihmisten auttaminen motivoi minua."
+    },
+    {
+        name: "Olli Tuomi",
+        title: "Arkkitehti",
+        picture: "faces/otuomi_small.jpg",
+        description: "Olen harrastanut ohjelmointia yhdeksänvuotiaasta, ja ammatikseni olen tehnyt sitä yli kymmenen vuotta. Omaksun uusia asioita erittäin nopeasti, ja olen varsinainen hai keksimään erilaisia tapoja ratkaista ongelmia tietokoneiden avulla. Tunnen perinpohjin prosessit, joilla ohjelmistoja suunnitellaan ja valmistetaan hyvin (ja muutaman joilla sitä tehdään huonosti)."
+    },
+    {
+        name: "Panu Åkerman",
+        title: "Ohjelmistokehittäjä",
+        picture: "faces/pakerman_small.jpg",
+        description: "Olen tutkija ja propellihattu ja olen kiinnostunut jokapaikan tietotekniikasta, käytettävyydestä ja leikkisyydestä. Lisäksi olen myös ohjelmistokehittäjä: Minulla on pitkä ja monipuolinen tausta ohjelmistokehityksesä ja haluan intohimoisesti tehdä hyviä ja elegantteja ohjelmia joita on helppo ja hauska käyttää."
+    },
+];
+
+export default function Personnel() {
+
+    return (
+        <div class="space-y-16 m-16 font-light">
+            <For each={personnel}>{(person) =>
+                <div class="m-4 flex space-x-8 max-w-screen-md mx-auto">
+                    <div class="shrink-0">
+                        <div class="space-y-1 pt-1.5">
+                            <img src={person.picture} alt={person.name}/>
+                            <div class="text-end text-xl pt-4">{person.name}</div>
+                            <div class="text-end">{person.title}</div>
+                        </div>
+                    </div>
+                    <div>{person.description}</div>
+                </div>
+            }</For>
+        </div>
+    )
+}
