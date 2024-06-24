45aabbf7345594a10907bc61f9738a6c2d6a413d
Martti Soininen <martti.soininen@evident.fi>
2024-06-24 13:45:00 +0300

    Add "hamburger navi" for mobile

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import {useLocation} from "@solidjs/router";
-import {For} from "solid-js";
+import {createEffect, createSignal, For, onCleanup, Show} from "solid-js";
 
 interface Link {
     url: string,
@@ -7,7 +7,28 @@ interface Link {
 }
 
 export default function Nav() {
+    const [scrollPosition, setScrollPosition] = createSignal(0);
+    const [scrollDirection, setScrollDirection] = createSignal(0);
+    const [showHamburger, setShowHamburger] = createSignal(false);
+    const [showSideNavi, setShowSideNavi] = createSignal(false);
     const location = useLocation();
+
+    createEffect(() => {
+        const handleScroll = () => {
+            const previous = scrollPosition();
+            setScrollPosition(window.scrollY);
+            setScrollDirection(scrollPosition() - previous);
+            setShowHamburger(scrollDirection() < 0);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        // Cleanup the event listener when the component is unmounted
+        onCleanup(() => {
+            window.removeEventListener("scroll", handleScroll);
+        });
+    });
+    
     const active = (path: string) => {
         const pathname = normalizePath(location.pathname);
         return path === pathname ? "bg-amber-800" : "";
@@ -21,14 +42,38 @@ export default function Nav() {
         {url: "/contact", label: "Yhteystiedot"},
     ];
 
+    function toggleNavigation() {
+        setShowSideNavi(!showSideNavi());
+    }
+    
     return (
-        <nav class="bg-gradient-to-tr from-stone-900 to-stone-800 sticky top-[-1.5rem] z-10">
-            <div class="container flex items-center text-white px-4 md:px-10 font-light overflow-x-auto">
-                <For each={links}>{(link) =>
-                    <a class={`${item} ${active(link.url)} whitespace-nowrap`} href={link.url}>{link.label}</a>
-                }</For>
+        <>
+            <nav class="bg-gradient-to-tr from-stone-900 to-stone-800 sticky top-[-1.5rem] z-10 max-sm:hidden">
+                <div class="container flex items-center text-white px-4 md:px-10 font-light">
+                    <For each={links}>{(link) =>
+                        <a class={`${item} ${active(link.url)} whitespace-nowrap`} href={link.url}>{link.label}</a>
+                    }</For>
+                </div>
+            </nav>
+
+            <div class={`sm:hidden bottom-16 right-4 fixed rounded-full z-30
+                bg-orange-400/50 text-5xl
+                flex h-20 w-20 justify-center items-center transition-opacity cursor-pointer 
+                ${showHamburger() ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}
+            onClick={() => toggleNavigation()}>
+                <span class="-translate-y-1">&#9776;</span>
             </div>
-        </nav>
+            
+            <div class={`${showSideNavi() ? '' : 'translate-x-full'} right-0 transition-transform fixed bg-gradient-to-tr from-stone-900 to-stone-800 h-full text-white z-20 drop-shadow-2xl`}>
+                <div class="text-xl font-light">
+                    <For each={links}>{(link) =>
+                        <a class={`${active(link.url)} whitespace-nowrap block p-8`} 
+                           onClick={() => toggleNavigation()}
+                           href={link.url}>{link.label}</a>
+                    }</For>
+                </div>
+            </div>
+        </>
     );
 }
 
