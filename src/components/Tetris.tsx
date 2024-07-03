96bbf6db356d9da129d2f1d42380a4dee295064f
Martti Soininen <martti.soininen@evident.fi>
2024-07-03 12:10:33 +0300

    Tweak button sizes

--- src/components/Tetris.tsx
+++ src/components/Tetris.tsx
@@ -638,7 +638,7 @@ function TetrisControlButton(props: TetrisButtonProps) {
     }
     
     return (
-        <button class="border border-amber-800 px-3 py-2 sm:px-6 sm:py-4 md:p-6 
+        <button class="border border-amber-800 px-3 py-2 sm:px-6 sm:py-4 md:p-6 min-w-12
         rounded active:bg-amber-200 transition-all select-none" 
                 onContextMenu={(e) => e.preventDefault()} 
                 onPointerDown={(e) => pointerDown(e)}
