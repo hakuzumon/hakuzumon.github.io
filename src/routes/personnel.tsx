9ae7064a832c2d187bc10c91e32df543041c8d84
Martti Soininen <martti.soininen@evident.fi>
2024-08-26 12:48:03 +0300

    Convert profile images to webp

--- src/routes/personnel.tsx
+++ src/routes/personnel.tsx
@@ -15,19 +15,19 @@ const dudes: Dude[] = [
     {
         name: "Henrik Huttunen",
         title: "Ohjelmistokehittäjä",
-        picture: "img/faces/2024/henrik3.jpg",
+        picture: "img/faces/2024/henrik3.webp",
         description: "Innostun uuden luomisesta ja ongelmien ratkaisemisesta. Pyrin tekemään vankasti toimivia ratkaisuja, joita on sekä miellyttävä käyttää että jatkokehittää.",
     },
     {
         name: "Jussi Koiranen",
         title: "Ohjelmistokehittäjä",
-        picture: "img/faces/2024/jussi1.jpg",
+        picture: "img/faces/2024/jussi1.webp",
         description: "Minulla on yli kahdenkymmenen vuoden työkokemus vaihtelevista ohjelmistohankkeista. Nautin aidosti työstäni ja pyrin jatkuvasti oppimaan uutta, mikä ei kuitenkaan tarkoita jatkuvasti uuden teknologian perässä juoksemista. Viihdyn erityisesti projekteissa, jotka haastavat pohtimaan ratkaisuja yhteistyössä eri alojen asiantuntijoiden kanssa. Toinen mielenkiinnon kohteeni on vanhojen sovellusten inkrementaalinen modernisointi ja kehittäminen."
     },
     {
         name: "Juha Komulainen",
         title: "Arkkitehti",
-        picture: "img/faces/2024/komu1.jpg",
+        picture: "img/faces/2024/komu1.webp",
         description: <>
             <p>Olen kokenut ohjelmistoarkkitehti Tampereelta. Koodaaminen on ollut osa elämääni jo lapsuudesta asti.
                 Vuodesta 2001 asti olen toiminut ammattilaisena, suunnitellen ja toteuttaen monipuolisia
@@ -42,37 +42,37 @@ const dudes: Dude[] = [
     {
         name: "Aki Loponen",
         title: "Ohjelmistokaiffari",
-        picture: "img/faces/2024/aki3.jpg",
+        picture: "img/faces/2024/aki3.webp",
         description: "Minulle mieluisin ohjelmistokehityksen pelipaikka ja rooli on keskikentällä kolmen neljänneksen taiteilijana ja toki noin kahdenkymmenen vuoden rypistys alalla on totuttanut pelaamaan paikkaa kuin paikkaa. Peliälyä ja -silmää riittää. Vähän tikitakaa, luovaa hulluutta ja sen jälkeen veräjän säppi kiinni ja nostaan maljaa."
     },
     {
         name: "Janne Mäki",
         title: "Ohjelmistokehittäjä",
-        picture: "img/faces/2024/janne2.jpg",
+        picture: "img/faces/2024/janne2.webp",
         description: "Pidän tärkeänä pysyä kehityksessä ajan hermolla ja opiskella uusia asioita ja teknologioita. Oman osaamiseni kehittäminen onkin minulle myös tärkeä harrastus polkupyöräilyn ohella. Teknologiat eivät kuitenkaan ole hopealuoti ohjelmistokehityksen ongelmiin, koska kaikki alkaa tarpeiden syvällisestä ymmärtämisestä ja selkeän ylläpidettävän koodin tuottamisesta."
     },
     {
         name: "Tero Poikonen",
         title: "Ohjelmistokehittäjä",
-        picture: "img/faces/2024/tero1.jpg",
+        picture: "img/faces/2024/tero1.webp",
         description: "Olen tehnyt arkkitehtuurikonsultointia ja määrittelytyötä. Olen työskennellyt ohjelmistosuunnittelijana, projektipäällikkönä ja loppukäyttäjien koulutuksessa. Olen kirjoittanut käyttöohjeita ja teknistä dokumentaatiota."
     },
     {
         name: "Martti Soininen",
         title: "Ohjelmistokehittäjä",
-        picture: "img/faces/2024/martti1.jpg",
+        picture: "img/faces/2024/martti1.webp",
         description: "Olen koodannut web-sovelluksia ammatikseni vuodesta 2008 (full-stack). Yritän aina selvittää ongelman perimmäisen syyn ja ratkaista sen. Saatan uppoutua mielenkiintoisiin algoritmisiin ongelmiin pitkäksikin aikaa, mutta pidän myös tiimityöskentelystä ja olen toiminut tutorinakin. Toisten ihmisten auttaminen motivoi minua."
     },
     {
         name: "Olli Tuomi",
         title: "Arkkitehti",
-        picture: "img/faces/2024/olli1.jpg",
+        picture: "img/faces/2024/olli1.webp",
         description: "Olen harrastanut ohjelmointia yhdeksänvuotiaasta, ja ammatikseni olen tehnyt sitä yli kymmenen vuotta. Omaksun uusia asioita erittäin nopeasti, ja olen varsinainen hai keksimään erilaisia tapoja ratkaista ongelmia tietokoneiden avulla. Tunnen perinpohjin prosessit, joilla ohjelmistoja suunnitellaan ja valmistetaan hyvin (ja muutaman joilla sitä tehdään huonosti)."
     },
     {
         name: "Panu Åkerman",
         title: "Ohjelmistokehittäjä",
-        picture: "img/faces/2024/panu2.jpg",
+        picture: "img/faces/2024/panu2.webp",
         description: "Olen tutkija ja propellihattu ja olen kiinnostunut jokapaikan tietotekniikasta, käytettävyydestä ja leikkisyydestä. Lisäksi olen myös ohjelmistokehittäjä: Minulla on pitkä ja monipuolinen tausta ohjelmistokehityksesä ja haluan intohimoisesti tehdä hyviä ja elegantteja ohjelmia joita on helppo ja hauska käyttää."
     },
 ];
